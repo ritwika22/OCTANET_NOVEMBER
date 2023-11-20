405cce8c2e3ceaed62bfc6d4e2ce52e9705c1e58
Ritwika Chakraborty <112366555+ritwika22@users.noreply.github.com>
2023-11-20 17:50:34 +0530

    Add files via upload

--- Task2/script.js
+++ Task2/script.js
@@ -12,6 +12,7 @@ function addTask() {
         var task = {
             title: val,
             checked: false,
+            priority: "medium"
         }
         allTask.push(task);
     }
@@ -22,21 +23,51 @@ function addTask() {
 function createTask() {
     listContainer.innerHTML = "";
     content = ""
+    allTask.sort(function(a, b){
+        prio = {
+            high: 0,
+            medium: 1,
+            low: 2
+        };
+        x = prio[a.priority];
+        if(a.checked){
+            x += 5;
+        }
+        y = prio[b.priority];
+        if(b.checked){
+            y += 5;
+        }
+        return x - y;
+    });
     for (i = 0; i < allTask.length; i++) {
-        if(allTask[i].checked){
-            content += "<li class=\"checked\">"
+        content += "<li class=\"" + allTask[i].priority
+        if (allTask[i].checked) {
+            content += " checked "
         }
-        else{
-            content += "<li>"
-        }
-        content += "<div class=\"check-div\"> \
+        content += "\"><div class=\"check-div\"> \
                     <span class=\"material-symbols-outlined\"> \
                     check_circle \
                     </span> \
                     </div> \
                     <p>" +
-                        allTask[i].title +
+                    allTask[i].title +
                     "</p> \
+                    <div class=\"prio-div\"> \
+                    <span class=\"material-symbols-outlined\"> \
+                    circle \
+                    </span> \
+                    <div class=\"hover\"> \
+                        <span class=\"material-symbols-outlined priority-h high\"> \
+                        circle \
+                        </span> \
+                        <span class=\"material-symbols-outlined priority-m medium\"> \
+                        circle \
+                        </span> \
+                        <span class=\"material-symbols-outlined priority-l low\"> \
+                        circle \
+                        </span> \
+                    </div> \
+                    </div> \
                     <div class=\"edit-div\"> \
                     <span class=\"material-symbols-outlined\"> \
                     border_color \
@@ -69,26 +100,38 @@ function addEventListenerToAllTask() {
     for (i = 0; i < edt_div.length; i++) {
         edt_div[i].addEventListener("click", function (e) { edtTask(e); });
     }
+
+    var edt_div = document.getElementsByClassName("priority-h");
+    for (i = 0; i < edt_div.length; i++) {
+        edt_div[i].addEventListener("click", function (e) { modifyPriority(e, "high"); });
+    }
+    var edt_div = document.getElementsByClassName("priority-m");
+    for (i = 0; i < edt_div.length; i++) {
+        edt_div[i].addEventListener("click", function (e) { modifyPriority(e, "medium"); });
+    }
+    var edt_div = document.getElementsByClassName("priority-l");
+    for (i = 0; i < edt_div.length; i++) {
+        edt_div[i].addEventListener("click", function (e) { modifyPriority(e, "low"); });
+    }
 }
 
-function addCheck(event){
-    console.log(event);
+function addCheck(event) {
     li = event.target.parentNode;
     p = li.getElementsByTagName("p")[0];
-    for(i=0;i<allTask.length;i++){
-        if(p.innerHTML == allTask[i].title){
+    for (i = 0; i < allTask.length; i++) {
+        if (p.innerHTML == allTask[i].title) {
             allTask[i].checked = !allTask[i].checked;
         }
     }
     createTask();
 }
 
-function delTask(event){
+function delTask(event) {
     li = event.target.parentNode;
     p = li.getElementsByTagName("p")[0];
     index_to_del = 0;
-    for(i=0;i<allTask.length;i++){
-        if(p.innerHTML == allTask[i].title){
+    for (i = 0; i < allTask.length; i++) {
+        if (p.innerHTML == allTask[i].title) {
             index_to_del = i;
             break;
         }
@@ -97,9 +140,20 @@ function delTask(event){
     createTask();
 }
 
-function edtTask(event){
+function edtTask(event) {
     li = event.target.parentNode;
     p = li.getElementsByTagName("p")[0];
     inputBox.value = p.innerHTML;
     delTask(event);
+}
+
+function modifyPriority(event, priority){
+    li = event.target.parentNode.parentNode.parentNode;
+    p = li.getElementsByTagName("p")[0];
+    for (i = 0; i < allTask.length; i++) {
+        if (p.innerHTML == allTask[i].title) {
+            allTask[i].priority = priority;
+        }
+    }
+    createTask();
 }
